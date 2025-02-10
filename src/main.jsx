aef8d23c7444d03dd4709c0b128dfd2adde2d159
Nikhil1141 <yepuri.nikhileswar@gmail.com>
2025-02-10 18:09:48 +0530

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import ReactDom from 'react-dom/client'
+import { BrowserRouter as Router } from 'react-router-dom'
+import App from './App'
+import './index.css'
+import { PrivyProvider } from '@privy-io/react-auth';
+
+// privy auth provider
+const root = ReactDom.createRoot(document.getElementById('root'))
+
+root.render(
+    <PrivyProvider
+        appId="cm6z0u88t00yxpt2727ktzfym"
+        config={{
+            // Customize Privy's appearance in your app
+            appearance: {
+                theme: 'dark',
+            },
+        }}
+    >
+        <Router>
+            <App />
+        </Router>
+    </PrivyProvider>
+)
