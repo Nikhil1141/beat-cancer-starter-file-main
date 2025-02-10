0c53e32ae2ba11b30a6685ff420e86ccabab10dd
Nikhil1141 <yepuri.nikhileswar@gmail.com>
2025-02-10 20:51:51 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,26 @@
-import React from 'react'
+import React, { memo } from 'react'
 import { Route, Routes } from 'react-router-dom'
+import Sidebar from './components/Sidebar'
+import Navbar from './components/Navbar'
+import { Home } from './pages'
 
 
 const App = () => {
     return (
-        <div className='relative flex min-h-screen flex-row bg-[#13131a] p-4'>
+        <div className='sm:-8 relative flex min-h-screen flex-row bg-[#13131a] p-4'>
             <div className='relative mr-10 hidden sm:flex'>
                 {/*sidebar*/}
+                <Sidebar />
             </div>
             <div className='mx-auto max-w-[1280px] flex-1 max-sm:w-full sm:pr-5'>
                 {/*Navbar*/}
+                <Navbar />
                 <Routes>
-                    <Route path='/' element={<div>Home page</div>} />
+                    <Route path='/' element={<Home />} />
                 </Routes>
             </div>
         </div>
     )
 }
 
-export default App
+export default memo(App)
